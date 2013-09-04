58da1de405d69706f1b0a87c3b094d0bcc735260
RendaniTshinakaho <r.nkhumeleni@gmail.com>
2013-09-04 16:03:18 +0200

    Added Tests

---
+++ BrownBagRabbitMqDemo/BB.Infrastructure.Tests/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BB.Infrastructure.Receiver;
+using NUnit.Framework;
+using BB.Infrastructure.Sender;
+
+namespace BB.Infrastructure.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void CanInstantiateSender()
+        {
+            var result = new SendRequest();
+            Assert.That(result,Is.Not.Null);
+        }
+        [Test]
+        public void CanInstantiateReceiver()
+        {
+            var result = new Receive();
+            Assert.That(result,Is.Not.Null);
+        }
+    }
+}
